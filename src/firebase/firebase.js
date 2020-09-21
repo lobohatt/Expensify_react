9a776e534b7caf194fea3cdf5c21239fddc6f61f
sourabhnaidu174 <lobothekiller48@gmail.com>
2020-09-22 00:43:10 +0530

    Expensify

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,6 +15,8 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+export { firebase, database as default };
+
 // Writing to database ****
 
 /*database.ref().set({
@@ -42,14 +44,143 @@ database.ref('attributes').set({
 
 // Removing from database
 
-database.ref('age')
+/*database.ref('age')
 .remove()
 .then(()=>{
 console.log('Data was removed');
 }).catch((e)=>{
 console.log('Did not remove data',e);
 });
-
+*/
 //or by using set
 
 //database.ref('age').set(null);
+
+
+//TO Update
+/*database.ref().update({
+  name:'Mike',
+  age:20,
+})*/
+
+//To Update and add new data
+/*database.ref().update({
+  name: 'Mike',
+  age: 20,
+  job: 'Software Developer',
+})*/
+
+//To Update // Add new //  As well as Delete
+/*database.ref().update({
+  name: 'Mike',
+  age: 20,
+  job: 'Software Developer',
+  isSingle: null
+});*/
+
+/*database.ref().update({
+  name: 'Mike',
+  age: 20,
+  job: 'Software Developer',
+  'location/city':'Boston'
+})
+
+
+database.ref('location/city')
+.once('value')
+.then((snapshot)=>{
+const val = snapshot.val();
+console.log(val);
+}).catch((e)=>{
+  console.log('Error fetching data', e)
+});
+
+database.ref().on('value',(snapshot) => {
+  console.log(snapshot.val());
+});
+
+database.ref().off();
+
+const onValueChange = (snapshot)=>{
+console.log(snapshot.val());
+}
+
+database.ref().on('value',onValueChange);
+database.ref().off(onValueChange);
+
+const onValueChangee = database.ref().on('value',(snapshot)=>{
+  console.log(snapshot.val());
+});
+
+
+database.ref().off(onValueChangee)
+
+database.ref('expenses')
+.once
+.then((snapshot)=>{
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot)=>{
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+  console.log(expenses);
+})
+
+
+database.ref('expenses').on('value',(snapshot)=>{
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+
+  console.log(expenses);
+})
+
+
+// Three main methods
+
+database.ref('expenses').on('child_removed', (snapshot) => {
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+
+  console.log(expenses);
+})
+
+database.ref('expenses').on('child_changed', (snapshot) => {
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+
+  console.log(expenses);
+})
+
+database.ref('expenses').on('child_added', (snapshot) => {
+  const expenses = [];
+
+  snapshot.forEach((childSnapshot) => {
+    expenses.push({
+      id: childSnapshot.key,
+      ...childSnapshot.val()
+    })
+  })
+
+  console.log(expenses);
+})*/
